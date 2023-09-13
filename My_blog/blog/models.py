55bfc57df222ae297b0ea59163439c14c98daceb
HpZT <e_w_7@mail.ru>
2023-09-13 11:32:55 +0500

    task 7

--- My_blog/blog/models.py
+++ My_blog/blog/models.py
@@ -5,9 +5,9 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    body = models.TextField()
+    title = models.CharField(max_length=200, verbose_name="Название")
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name="Автор")
+    body = models.TextField(verbose_name="Описание")
 
     def __str__(self):
         return self.title
